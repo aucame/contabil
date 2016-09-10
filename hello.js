442baad55bc3305a0d48f5856f5beb9a10b81ed6
aucame <aucame@hotmail.com>
2016-09-10 05:37:20 -0300

    consumindo api com angularjs

---
+++ hello.js
@@ -0,0 +1,21 @@
+
+var app = angular.module('demo', []);
+
+app.controller('Hello', function($scope, $http) {
+
+    var chamada = function () {
+        $http({
+        method: 'GET',
+        url: 'http://127.0.0.1:8080/usuarios'
+        }).then(function(response) {
+            console.log(response.data);
+            $scope.greeting = response.data;
+        }, function(response) {
+            console.log('Não foi possível obter os dados: ' + response.data);
+        });
+    }
+
+    chamada();
+
+});
+
