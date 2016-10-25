146b6ac6132a115050d7066f92f06ef091a0b4d3
Aurelio <aucame@hotmail.com>
2016-10-24 23:07:44 -0200

    login

--- appContabil.js
+++ appContabil.js
@@ -3,7 +3,8 @@ var app = angular.module('appContabil', []);
 
 app.controller('ctlContabil', function($scope, $location) {
 
-	$scope.principal = function() {
+	$scope.principal = function(user) {
+//		console.log(user)
 		location.href = 'http://' + $location.host() + '/contabil/principal.html';
 	};
 
