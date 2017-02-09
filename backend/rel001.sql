b1425890eb66bf18c0ba6dc13ae215a949b6755e
aucame <aucame@hotmail.com>
2017-02-09 21:22:19 -0200

    Relatorio 1.0

---
+++ backend/rel001.sql
@@ -0,0 +1,17 @@
+SELECT a.codigo, 
+       a.descricao,
+       (select b.valor 
+          from dbContabil.cadlancamento b
+		 where b.ano = 2015
+           and b.mes = 01
+           and b.idplano = a.codigo
+       ) Janeiro2015,
+       (select b.valor 
+          from dbContabil.cadlancamento b
+		 where b.ano = 2016
+           and b.mes = 01
+           and b.idplano = a.codigo
+       ) Janeiro2016       
+  FROM dbContabil.cadplano a
+order by a.tipocd desc
+;
