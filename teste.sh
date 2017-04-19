a1018249b31f22775b43300473f614dff4158e73
Aurelio <aucame@hotmail.com>
2017-04-19 16:41:50 -0300

    Deploy

--- teste.sh
+++ teste.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
 # scp -r /usr/share/nginx/html/contabil/bower_components root@200.98.174.103:/usr/share/nginx/html/contabil
-# scp -r backend/api root@200.98.174.103:/usr/share/nginx/html/contabil/backend
-# scp -r app/*.js root@200.98.174.103:/usr/share/nginx/html/contabil/app
 # scp *.* root@200.98.174.103:/usr/share/nginx/html/contabil
-# scp /usr/share/nginx/html/contabil/*.html root@200.98.174.103:/usr/share/nginx/html/contabil
+
+# Deploy App
+# scp -r app/*.js root@200.98.174.103:/usr/share/nginx/html/contabil/app
+
+# Deploy backend-api
+# scp -r backend/api root@200.98.174.103:/usr/share/nginx/html/contabil/backend
+
+# Deploy BackEnd
+# cd backend
+# scp *.py *.php *.xls *.pdf *.html *.json *.sql *.txt   root@200.98.174.103:/usr/share/nginx/html/contabil/backend
+# cd ..
+
+# Deploy HTML
+# scp *.html *.png *.json *.txt *.css *.sh  root@200.98.174.103:/usr/share/nginx/html/contabil
